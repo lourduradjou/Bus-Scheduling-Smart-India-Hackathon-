60e19fc13a3ca51220e9ae1b8a706d169568af0c
Lourdu Radjou <rajlourdu15@gmail.com>
2024-09-02 00:37:46 +0530

    SideBar updated

--- frontend/src/components/shared/SideBar.jsx
+++ frontend/src/components/shared/SideBar.jsx
@@ -48,22 +48,26 @@ const SideBar = () => {
 	return (
 		<div className='w-60 bg-[#00245c] text-gray-300  min-h-screen'>
 			<div className='w-full flex items-center justify-start p-8'>
-				<h1 className='text-2xl hover:text-[#ffc404] duration-300 font-semibold pl-2'>NCT Delhi</h1>
+				<h1 className='text-2xl hover:text-[#ffc404] duration-300 font-semibold pl-2'>
+					NCT Delhi
+				</h1>
 			</div>
 
 			<nav className='p-8'>
 				<ul className='flex flex-col gap-4'>
 					{SIDE_BAR_LINKS.map((link) => (
-						<li
-							className={`rounded-md flex gap-4 items-center p-3 cursor-pointer text-lg ${
-								themeClasses[link.theme] ||
-								'text-gray-300 hover:bg-[#6c757d]'
-							}`}
-							key={link.key}
-						>
-							{link.icon}
-							<span>{link.label}</span>
-						</li>
+						<a href={link.path} className='no-underline'>
+							<li
+								className={`rounded-md flex gap-4 items-center p-3 cursor-pointer text-lg ${
+									themeClasses[link.theme] ||
+									'text-gray-300 hover:bg-[#6c757d]'
+								}`}
+								key={link.key}
+							>
+								{link.icon}
+								<span>{link.label}</span>
+							</li>
+						</a>
 					))}
 				</ul>
 			</nav>
