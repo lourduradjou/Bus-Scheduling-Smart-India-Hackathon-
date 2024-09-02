6fff5e2ad8a5beacc0ccfe3b069c3e16c6a50990
Lourdu Radjou <rajlourdu15@gmail.com>
2024-09-02 15:18:06 +0530

    changed the folder strcuture of empManagement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashBoard from './pages/DashBoard';
 import Login from './auth/Login'; // Assuming you have a login page
 import SignUp from './auth/SignUp'; // Assuming you have a signup page
-import EmployeeManagement from './components/EmployeeManagement';
 import MainLayout from './components/shared/MainLayout';
 import EmployeeManagement from './pages/EmployeeManagement';
 
@@ -14,7 +13,6 @@ const App = () => {
         {/* Public routes (without sidebar) */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/EmployeeManagement" element={<EmployeeManagement />} />
 
         {/* Protected routes (with sidebar) */}
         <Route
